5731b97bb7ce7b99e06fa60da55262693332b4a3
daek03001 <70134255+daek03001@users.noreply.github.com>
2022-03-16 16:53:21 +0100

    Create JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,4 +1,23 @@
-window.addEventListener('scroll', () => {
+window.addEventListener('scroll', () => { 
     document.querySelector('nav').classList.toggle
     ('window-scroll', window.scrollY > 0) 
 })
+
+/* Faq Gömma, visa Svar */ 
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => { 
+        faq.classList.toggle('open');
+
+
+        /* Ändra icon på Faq */ 
+        const icon = faq.querySelector('.faq_icon i');
+        if(icon.className === 'uil uil-plus') {
+            icon.className = "uil uil-minus"
+        } else {
+            icon.className = "uil uil-plus"
+        }
+    })
+})
+
